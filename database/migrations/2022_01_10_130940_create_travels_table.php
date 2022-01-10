677c4bd4f28394acc4b9fc3ea6303e563d91689c
LeonardoCeccarelli <leo.ceccarelli@tiscali.it>
2022-01-10 14:17:37 +0100

    create migration for table "travels"

---
+++ database/migrations/2022_01_10_130940_create_travels_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTravelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('travels', function (Blueprint $table) {
+            $table->id();
+            $table->char("destination", 100);
+            $table->date("departure date", 100);
+            $table->date("arrival date", 100);
+            $table->decimal("price", 6, 2);
+            $table->char("description", 255);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('travels');
+    }
+}
